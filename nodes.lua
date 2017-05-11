a34de33961527522f503363768b0348ef030d0ac
Alex <bl00ntdmonkey@yahoo.com>
2017-05-11 13:22:05 -0700

    Changed > to >=
    Using > required a stack of 3 or more bread slices to toast. >= allows 2.

--- nodes.lua
+++ nodes.lua
@@ -254,7 +254,7 @@ minetest.register_craftitem("waffles:breadslice", {
 
 		if node.name == "homedecor:toaster"
 		or node.name == "waffles:toaster" then
-			if itemstack:get_count() > 2 then
+			if itemstack:get_count() >= 2 then
 				itemstack:take_item(2)
 				minetest.set_node(pos, {name = "waffles:toaster_with_breadslice", param2 = node.param2})
 			return itemstack
