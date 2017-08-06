dd73b86637a6aa759114432c6097c38f711a0f22
fat115 <fat115@framasoft.org>
2017-08-06 09:21:59 +0200

    missing S function in batter.lua

--- batter.lua
+++ batter.lua
@@ -1,3 +1,5 @@
+local S = waffles.intllib
+
 -- Waffle Batter
 local batter = {}
 
@@ -24,21 +26,21 @@ function batter.register(filled, original_def)
 		stack_max = 1,
 		groups = {not_in_creative_inventory = group},
 		on_use = function(itemstack, user, pointed_thing)
-	
+
 			if pointed_thing.type ~= "node" then
 				return
 			end
-	
+
 			local pos = pointed_thing.under
 			local pname = user:get_player_name()
-	
+
 			if minetest.is_protected(pos, pname) then
 				minetest.record_protection_violation(pos, pname)
 				return
 			end
-	
+
 			local node = minetest.get_node(pos)
-	
+
 			if node.name == "waffles:wafflemaker_open_empty" then
 
 				minetest.set_node(pos, {name = "waffles:wafflemaker_open_full", param2 = node.param2})
